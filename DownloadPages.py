9223a289e94249fb6458b7d4175abe2c343049b1
Tom Heng <uestcheng@gmail.com>
2015-07-27 22:23:52 +0800

    my dxo spider page
    use gevent scrapy

---
+++ DownloadPages.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python2.7
+#-*- coding: utf-8 -*-
+
+import scrapy
+
+
+class DownloadPages(object):
+    """dxopage downloads"""
+    def __init__(self):
---
+++ Items.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+class Products(scrapy.Item):
+    """"""
+
